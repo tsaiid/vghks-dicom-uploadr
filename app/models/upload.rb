fe49bf1b3d90d8cc734886e987606018755d016e
I-Ta Tsai <ittsai@gmail.com>
2013-07-19 22:05:24 +0800

    need to output id for download.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,11 +6,12 @@ class Upload < ActiveRecord::Base
 
   def to_jq_upload
     {
+      "id" => self.id,
       "name" => read_attribute(:upload_file_name),
       "size" => read_attribute(:upload_file_size),
       "url" => upload.url(:original),
       "delete_url" => upload_path(self),
-      "delete_type" => "DELETE" 
+      "delete_type" => "DELETE"
     }
   end
 
